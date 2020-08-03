225bbd12cd9ea6b1a21843f2919db2b22e35f40e
root <didguscjf95@naver.com>
2020-08-03 19:10:32 +0900

    create Gate and welcome screen

--- components/Gate.js
+++ components/Gate.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { logIn, logOut } from "../redux/usersSlice";
+import { NavigationContainer } from "@react-navigation/native";
+import Auth from "../navigation/Auth";
 
 export default () => {
   const { isLoggedIn } = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
   return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+    <NavigationContainer>
       {isLoggedIn ? (
         <TouchableOpacity onPress={() => dispatch(logOut())}>
           <Text>Log out</Text>
         </TouchableOpacity>
       ) : (
-        <TouchableOpacity onPress={() => dispatch(logIn("bs"))}>
-          <Text>Log in</Text>
-        </TouchableOpacity>
+        <Auth />
       )}
-    </View>
+    </NavigationContainer>
   );
 };
---
+++ navigation/Auth.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Welcome from "../screens/Welcome";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+import BackBtn from "../components/Auth/BackBtn";
+
+const Auth = createStackNavigator();
+
+export default () => (
+  <Auth.Navigator
+    headerMode="float"
+    mode="modal"
+    screenOptions={{
+      headerBackTitleVisible: false,
+      headerTransparent: true,
+      headerBackImage: <BackBtn />,
+    }}
+  >
+    <Auth.Screen
+      name="Welcome"
+      component={Welcome}
+      options={{
+        headerTitleStyle: {
+          color: "white",
+        },
+      }}
+    />
+    <Auth.Screen name="SignIn" component={SignIn}></Auth.Screen>
+    <Auth.Screen name="SignUp" component={SignUp}></Auth.Screen>
+  </Auth.Navigator>
+);
